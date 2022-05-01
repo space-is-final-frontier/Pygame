4de714815ee27938ae03326ce7d04a0b2b798db7
Tejas Jain <TejasJain05@outlook.com>
2022-05-01 20:32:40 +0530

    init

---
+++ Python Object Oriented Programming (POOP)/scope.py
@@ -0,0 +1,28 @@
+def update_health(amount):
+    health += amount
+
+health = 10
+print(health)
+
+
+
+class Hero:
+    def __init__(self, damage, monster):
+        self.damage = damage
+        self.monster = monster
+
+    def attack(self):
+        self.monster.get_damage(self.damage)
+
+class Monster:
+    def __init__(self, health):
+        self.health = health
+
+    def get_damage(self, amount):
+        self.health -= amount
+
+monster = Monster(100)
+hero = Hero(20, monster)
+
+hero.attack()
+print(monster.health)
