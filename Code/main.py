435eaa48b7c457905abc929c24c5317ac0341bf1
Tejas Jain <TejasJain05@outlook.com>
2022-06-08 20:12:02 +0530

    Slight changes before using rectangles

--- Code/main.py
+++ Code/main.py
@@ -1,25 +1,33 @@
+#Importing the modules
 import pygame
 from variables import *
 from sys import exit
-from animation import move
 
 pygame.init()   #Initialising pygame
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))   #Creating the display surface
+screen = pygame.display.set_mode((WIDTH, HEIGHT))   #Creating the display surface, i.e. the screen the player will see
 pygame.display.set_caption("Google Dino Pygame")    #Setting the caption
 
-clock = pygame.time.Clock()    #Creating the clock obimg_indect
+clock = pygame.time.Clock()    #Creating the clock object
 
-text_font = pygame.font.Font('Font\Pixeltype.ttf', 45)  #Creating the font
 
-sky_surface = pygame.image.load('Graphics\Sky.png').convert()    #Loading the various surfaces
-ground_surface = pygame.image.load('Graphics\Ground.png').convert()    
-fps_counter = text_font.render('FPS: 60', False, 'black')    
+text_font = pygame.font.Font('Font/Pixeltype.ttf', 45)  #Creating the font
 
+sky_surface = pygame.image.load('Graphics/Sky.png').convert()    #Loading the various surfaces
+ground_surface = pygame.image.load('Graphics/Ground.png').convert()
+
+fps_counter = text_font.render('FPS: 60', False, 'black')   #Creating a fake fps counter
+
+
+imp_surface_unflipped = pygame.image.load('Graphics/Imp/walk_1.png').convert_alpha()
+imp_surface = pygame.transform.flip(imp_surface_unflipped, True, False)
+imp_xpos = 400
+imp_ypos = 175
+imp_dx = -3
 
 while True:
     
-    for event in pygame.event.get():
+    for event in pygame.event.get():    #Exit code
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
@@ -28,15 +36,14 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(fps_counter, (5, 5))
 
-    IMP_XPOS, IMP_YPOS, frames, img, img_ind = move(IMP_XPOS, IMP_YPOS, IMP_DX, IMP_DY, frames, img, img_ind)
 
-    if IMP_XPOS <= -100:
-        IMP_XPOS = 800
+    imp_xpos += imp_dx
 
-    imp_surface_unflipped = pygame.image.load('Graphics\Imp\walk_{}.png'.format(img)).convert_alpha()
-    imp_surface = pygame.transform.flip(imp_surface_unflipped, False, False)
+    if imp_xpos <= -100:
+        imp_xpos = 800
 
-    screen.blit(imp_surface, (IMP_XPOS, IMP_YPOS))
+    screen.blit(imp_surface, (imp_xpos, imp_ypos))
 
-    pygame.display.update()
+
+    pygame.display.update()     #Updating the display surface
     clock.tick(60)
--- Code/variables.py
+++ Code/variables.py
@@ -1,14 +1,2 @@
 WIDTH = 800
 HEIGHT = 400
-
-IMP_XPOS = 400
-IMP_YPOS = 175
-
-IMP_DX = -3
-IMP_DY = 0
-
-WALK_LIST = [1, 2, 3, 4, 5, 6]
-
-frames = 0
-img_ind = 0
-img = WALK_LIST[0]
