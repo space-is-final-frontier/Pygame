937a01f087b8874f7aab8a663422708c334de4a8
Tejas Jain <TejasJain05@outlook.com>
2022-06-09 10:24:45 +0530

    Several changes before adding keyboard inputs

--- Code/main.py
+++ Code/main.py
@@ -17,13 +17,17 @@ sky_surface = pygame.image.load('Graphics/Sky.png').convert()    #Loading the va
 ground_surface = pygame.image.load('Graphics/Ground.png').convert()
 
 fps_counter = text_font.render('FPS: 60', False, 'black')   #Creating a fake fps counter
+fps_rect = fps_counter.get_rect(topleft = (5, 5))
 
 
-imp_surface_unflipped = pygame.image.load('Graphics/Imp/walk_1.png').convert_alpha()
+imp_surface_unflipped = pygame.image.load('Graphics/Imp/walk_1_mod.png').convert_alpha()
 imp_surface = pygame.transform.flip(imp_surface_unflipped, True, False)
-imp_xpos = 400
-imp_ypos = 175
-imp_dx = -3
+imp_rect = imp_surface.get_rect(bottomleft = (400, 300))
+
+
+vik_surface = pygame.image.load('Graphics/Viking Axe/walk_1_mod.png').convert_alpha()
+vik_rect = vik_surface.get_rect(bottomleft = (80, 300))
+
 
 while True:
     
@@ -31,18 +35,42 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        #if event.type == pygame.MOUSEMOTION:
+        #    print(vik_rect.collidepoint(event.pos))
     
+
+    #Placing the background and "fps counter"
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(fps_counter, (5, 5))
+    screen.blit(fps_counter, fps_rect)
 
 
-    imp_xpos += imp_dx
+    #pygame.draw.line(screen, 'black', (0, 0), (800, 400), 10)
+    #pygame.draw.line(screen, 'black', (0, 0), pygame.mouse.get_pos(), 10)
+    #pygame.draw.ellipse(screen, 'gold', pygame.Rect(200, 300, 100, 100))
 
-    if imp_xpos <= -100:
-        imp_xpos = 800
 
-    screen.blit(imp_surface, (imp_xpos, imp_ypos))
+    #Placing the Imp enemy and moving it
+    imp_rect.left += -3
+
+    if imp_rect.right <= 0:
+        imp_rect.left = 800
+
+    screen.blit(imp_surface, imp_rect)
+
+
+    #Placing the Viking Hero
+    screen.blit(vik_surface, vik_rect)
+
+
+    #print(vik_rect.colliderect(imp_rect))
+
+    #mouse_pos = pygame.mouse.get_pos()
+    #print(vik_rect.collidepoint(mouse_pos))
+
+    #print(pygame.mouse.get_pressed())
+
 
 
     pygame.display.update()     #Updating the display surface
