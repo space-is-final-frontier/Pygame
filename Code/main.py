7156261e9e4f8e9892ea07315c1aa562a6dd5cdf
Tejas Jain <TejasJain05@outlook.com>
2022-06-11 15:38:19 +0530

    Multiple enemies and Double jump

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,7 @@
 #Importing the modules
 import pygame
 from sys import exit
+from random import randint
 
 def display_fps():
     fps = int(clock.get_fps())
@@ -16,9 +17,33 @@ def display_score():
     score_rect = score_counter.get_rect(center = (400, 50))
     screen.blit(score_counter, score_rect)
 
+def imp_movement(imp_list):
+    if imp_list:
+
+        for imp_rect in imp_list:
+
+            imp_rect.x -= 3.5
+
+            screen.blit(imp_surface, imp_rect)
+    
+        imp_list = [imp_rect for imp_rect in imp_list if imp_rect.x > -50]
+
+    return imp_list
+
+def collisions(viking, imps_list):
+    global game_active
+
+    if imps_list:
+
+        for imps_rect in imps_list:
+
+            if viking.colliderect(imps_rect):
+
+                game_active = False
+
 pygame.init()   #Initialising pygame
 
-#global variables
+#Global variables
 screen = pygame.display.set_mode((800, 400))   #Creating the display surface, i.e. the screen the player will see
 pygame.display.set_caption("Google Dino Pygame")    #Setting the caption
 clock = pygame.time.Clock()    #Creating the clock object
@@ -36,7 +61,9 @@ ground_surface = pygame.image.load('Graphics/Ground.png').convert()
 imp_surface = pygame.image.load('Graphics/Imp/ready_1_mod.png').convert_alpha()
 imp_surface = pygame.transform.flip(imp_surface, True, False)
 imp_surface = pygame.transform.scale(imp_surface, (54, 90))
-imp_rect = imp_surface.get_rect(bottomleft = (400, 300))
+
+
+imp_rect_list = []
 
 
 vik_surface = pygame.image.load('Graphics/Viking Axe/ready_1_mod.png').convert_alpha()
@@ -55,9 +82,14 @@ instruct = text_font.render("Press space bar to start the game", False, 'black')
 instruct_rect = instruct.get_rect(center = (400, 350))
 
 
+#Obstacles
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
+
 while True:
     
-    for event in pygame.event.get():    #Exit code
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
@@ -65,16 +97,21 @@ while True:
         if game_active:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if vik_rect.collidepoint(event.pos) and vik_rect.bottom >= 290:
-                    vik_gravity = -20
+                    vik_gravity = -19
         
             if event.type == pygame.KEYDOWN:
                 if event.key in (pygame.K_SPACE, pygame.K_k, pygame.K_UP) and vik_rect.bottom >= 290:
-                    vik_gravity = -20
+                    vik_gravity = -19
+
+                if event.key in (pygame.K_SPACE, pygame.K_k, pygame.K_UP) and vik_rect.bottom < 200:
+                    vik_gravity -= 6
+            
+            if event.type == obstacle_timer:
+                imp_rect_list.append(imp_surface.get_rect(bottomleft = (randint(900, 1100), 300)))
             
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                imp_rect.x = 400
                 start_time = pygame.time.get_ticks()
 
 
@@ -87,12 +124,7 @@ while True:
 
 
         #Placing the Imp enemy and moving it
-        imp_rect.left += -3.5
-
-        if imp_rect.right <= 0:
-            imp_rect.left = 800
-
-        screen.blit(imp_surface, imp_rect)
+        imp_rect_list = imp_movement(imp_rect_list)
 
 
         #Placing the Viking Hero
@@ -101,16 +133,25 @@ while True:
 
         if vik_rect.bottom >= 300:
             vik_rect.bottom = 300
+
+        if vik_rect.top <= 0:
+            vik_rect.top = 0
+
         screen.blit(vik_surface, vik_rect)
 
+
         #Collisions
-        if imp_rect.colliderect(vik_rect):
-            game_active = False
+        collisions(vik_rect, imp_rect_list)
+
 
     else:
         screen.fill((94, 129, 162))
         screen.blit(intro_vik, intro_vik_rect)
         screen.blit(instruct, instruct_rect)
+        imp_rect_list.clear()
+
+        vik_rect.bottom = 300
+        vik_gravity = 0
 
         end_score = text_font.render(f"Score: {score}", False, 'black')
         end_score_rect = end_score.get_rect(center = (400, 50))
@@ -119,6 +160,7 @@ while True:
             pass
         else:
             screen.blit(end_score, end_score_rect)
+
 
     pygame.display.update()     #Updating the display surface
     clock.tick(60)
